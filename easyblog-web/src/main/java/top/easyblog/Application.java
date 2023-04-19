2400bb8c179c3e7ea88f34654e7bb2df5991d76b
frank.huang <2489868503@qq.com>
2023-04-19 22:01:15 +0800

    feat: Try run success!

--- easyblog-web/src/main/java/top/easyblog/Application.java
+++ easyblog-web/src/main/java/top/easyblog/Application.java
@@ -1,5 +1,7 @@
 package top.easyblog;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -7,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableAspectJAutoProxy
 @EnableWebMvc
+@MapperScans({
+		@MapperScan("top.easyblog.dao")
+})
 @SpringBootApplication(scanBasePackages = {"top.easyblog"})
 public class Application {
 
